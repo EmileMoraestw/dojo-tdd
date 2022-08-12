ef5ac7424b27090fb4b4e18e6303aaf79b71c4c3
EmileMoraes <emile.moraes@thoughtworks.com>
2022-08-11 22:06:06 -0300

    TDD done
    TDD done

--- JAVA/CalculatorTDD/src/main/java/org/example/Calculator.java
+++ JAVA/CalculatorTDD/src/main/java/org/example/Calculator.java
@@ -2,4 +2,29 @@ package org.example;
 
 public class Calculator {
 
+public int sum(int number1, int number2){
+    return number1 + number2;
+
+}
+
+    public int subtract(int number1, int number2){
+        return number1 - number2;
+
+    }
+    public int multiplicate(int number1, int number2){
+        return number1 * number2;
+
+    }
+
+
+    public int divide(int number1, int number2){
+        return number1 / number2;
+
+    }
+
+    public int validade(int number1, int number2){
+        int value = sum(number1, number2);
+
+    }
+
 }
--- JAVA/CalculatorTDD/src/test/java/org/example/CalculatorTest.java
+++ JAVA/CalculatorTDD/src/test/java/org/example/CalculatorTest.java
@@ -8,5 +8,36 @@ import static org.junit.Assert.*;
 
 public class CalculatorTest {
 
+    Calculator calculator;
 
+    @Before
+    public void startCalculator(){
+        calculator = new Calculator();
+    }
+    @Test
+    public void shouldReturnSum() {
+
+        Assert.assertEquals(7 , calculator.sum(4, 3));
+    }
+    @Test
+    public void shouldReturnSubtract() {
+
+        Assert.assertEquals(1 , calculator.subtract(4, 3));
+    }
+
+    @Test
+    public void shouldReturnMultiplicate() {
+
+        Assert.assertEquals(12 , calculator.multiplicate(4, 3));
+    }
+    @Test
+    public void shouldReturnDivide() {
+
+        Assert.assertEquals(5 , calculator.divide(10, 2));
+    }
+
+    @Test
+    public void shouldSumAndSubtract(){
+        Assert.assertEquals(2, calculator.validade(20,10));
+    }
 }
